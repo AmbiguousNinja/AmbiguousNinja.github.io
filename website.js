625aed28dfbcfa9b27689688313fae0be2f2e34d
AmbiguousNinja <theambiguousninja@gmail.com>
2014-04-03 16:49:57 -0400

    Create website.js

---
+++ website.js
@@ -0,0 +1,16 @@
+console.log("HOLY FUCK IS THIS A REAL WEBSITE?");
+realSite = prompt("WELL IS IT?(y/n)");
+
+if(realSite = "y"){
+  console.log("DAMN STRAIGHT");
+}
+
+else if(realSite = "n"){
+  console.log("GO PLAY COD YOU ASSHOLE");
+}
+
+else{
+  console.log("SPEAK ENGLISH");
+}
+
+console.log("Well, this is it, nothing else to display.");
